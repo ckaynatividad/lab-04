ed55506a5266ff7c3b7dee40a1251e214c699daf
ckaynatividad <natividad.ckay@gmail.com>
2021-09-30 16:08:13 -0700

    stylized buttons background and text

--- app.js
+++ app.js
@@ -10,4 +10,50 @@ addButton.addEventListener('click', ()=>{
     const result = add(value1, value2);
     console.log(result);
     addAnswer.textContent = result;
+    addAnswer.classList.add('final');
+});
+
+import { sub } from './calculations.js';
+const subInput1 = document.getElementById('sub-input-1');
+const subInput2 = document.getElementById('sub-input-2');
+const subButton = document.getElementById('sub-btn');
+const subAnswer = document.getElementById('sub-output');
+
+subButton.addEventListener('click', ()=>{
+    const value1 = Number(subInput1.value);
+    const value2 = Number(subInput2.value);
+    const result = sub(value1, value2);
+    console.log(result);
+    subAnswer.textContent = result;
+    subAnswer.classList.add('final');
+});
+
+import { mul } from './calculations.js';
+const mulInput1 = document.getElementById('mul-input-1');
+const mulInput2 = document.getElementById('mul-input-2');
+const mulButton = document.getElementById('mul-btn');
+const mulAnswer = document.getElementById('mul-output');
+
+mulButton.addEventListener('click', ()=>{
+    const value1 = Number(mulInput1.value);
+    const value2 = Number(mulInput2.value);
+    const result = mul(value1, value2);
+    console.log(result);
+    mulAnswer.textContent = result;
+    mulAnswer.classList.add('final');
+});
+
+import { div } from './calculations.js';
+const divInput1 = document.getElementById('div-input-1');
+const divInput2 = document.getElementById('div-input-2');
+const divButton = document.getElementById('div-btn');
+const divAnswer = document.getElementById('div-output');
+
+divButton.addEventListener('click', ()=>{
+    const value1 = Number(divInput1.value);
+    const value2 = Number(divInput2.value);
+    const result = div(value1, value2);
+    console.log(result);
+    divAnswer.textContent = result;
+    divAnswer.classList.add('final');
 });
--- calculations.js
+++ calculations.js
@@ -5,3 +5,18 @@ export function add_two(num){
 export function add(num1, num2) {
     return num1 + num2;
 }
+
+
+export function sub(num1, num2) {
+    return num1 - num2;
+}
+
+
+export function mul(num1, num2) {
+    return num1 * num2;
+}
+
+
+export function div(num1, num2) {
+    return num1 / num2;
+}
