98110725632c2561508c9264cc7591cdf3d43236
ckaynatividad <natividad.ckay@gmail.com>
2021-10-01 11:15:21 -0700

    added testing for first four functions

--- test/example.test.js
+++ test/example.test.js
@@ -3,16 +3,49 @@
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
+import { add } from '/..calculations.js';
+import { sub } from '/..calculations.js';
+import { mul } from '/..calculations.js';
+import { div } from '/..calculations.js';
+
+
+test('add', (expect) => {
+    const input1 = 2;
+    const input2 = 2;
+    const expected = 4;
+
+    const actual = add(input1, input2);
+
+    expect.equal(actual, expected);
+});
+
+test('sub', (expect) => {
+    const input1 = 2;
+    const input2 = 2;
+    const expected = 0;
+
+    const actual = sub(input1, input2);
+
+    expect.equal(actual, expected);
+});
+
+test('mul', (expect) => {
+    const input1 = 2;
+    const input2 = 3;
+    const expected = 6;
+
+    const actual = mul(input1, input2);
+
+    expect.equal(actual, expected);
+});
+
+test('div', (expect) => {
+    const input1 = 2;
+    const input2 = 2;
+    const expected = 1;
+
+    const actual = div(input1, input2);
+
     expect.equal(actual, expected);
 });
